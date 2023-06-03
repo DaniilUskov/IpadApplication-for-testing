95dd6ef12f03fc3316edad0a4fe7c4ed9190f375
Daniil <u_danila@mail.ru>
2023-06-03 18:05:47 +0500

    Версия 1.23.06.02 Добавлена карта

--- IpadApplication/IpadInformationViewController.swift
+++ IpadApplication/IpadInformationViewController.swift
@@ -4,39 +4,56 @@
 //
 //  Created by Даниил Усков on 03.06.2023.
 //
-
-import UIKit
 import CoreLocation
+import UIKit
+import MapKit
 
 class IpadInformationViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var GeopositionVIew: UIView!
-    let locationManager = CLLocationManager()
+    @IBOutlet weak var mapInformation: UILabel!
+    
+    private let map:MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        GeopositionVIew.addSubview(map)
+        title = "Home"
+        self.mapInformation.text = "Загрузка..."
         
-        locationManager.delegate = self
+        LocationManager.shared.gerUserLocation{ [weak self] location in
+            DispatchQueue.main.async{
+                guard let strongSelf = self else {
+                    return
+                }
+                strongSelf.addMapPin(with: location)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        map.frame = GeopositionVIew.bounds
+        map.frame.size.height = GeopositionVIew.bounds.height
+        map.frame.size.width = GeopositionVIew.bounds.width
+    }
+    
+    func addMapPin(with location: CLLocation){
+        let pin = MKPointAnnotation()
+        pin.coordinate = location.coordinate
+        map.setRegion(MKCoordinateRegion(center: location.coordinate,
+                                                    span: MKCoordinateSpan(
+                                                        latitudeDelta: 0.7,
+                                                        longitudeDelta: 0.7)),
+                                 animated:true)
+        map.addAnnotation(pin)
         
-        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-        locationManager.requestWhenInUseAuthorization()
-        
-        locationManager.startUpdatingLocation()
-        
-        
-        
-        /*override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-         super.viewWillTransition(to: size, with: coordinator)
-         if UIDevice.current.orientation.isLandscape {
-         GeopositionVIew.backgroundColor = .blue
-         
-         } else {
-         GeopositionVIew.backgroundColor = .red
-         }
-         }*/
-        
-        // LocationManager Delegates
-        
-        
+        LocationManager.shared.resolveLocationName(with: location) { [weak self] locationName in
+            self?.mapInformation.text = locationName
+        }
     }
 }
+
